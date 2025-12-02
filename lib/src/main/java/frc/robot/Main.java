65c7fc75df2c2d80166432fceeca5fa177ad40c5
aopalka <79727669+aopalka@users.noreply.github.com>
2025-12-01 21:43:42 -0500

    Fixing checkstyle issues with lib.

--- lib/src/main/java/frc/robot/Main.java
+++ lib/src/main/java/frc/robot/Main.java
@@ -6,14 +6,13 @@ package frc.robot;
  * call.
  */
 public final class Main {
-    private Main() {}
-  
-    /**
-     * Main initialization function. Do not perform any initialization here.
-     *
-     * <p>If you change your main robot class, change the parameter type.
-     */
-    public static void main(String... args) {
-    }
-  }
-  
+  private Main() {}
+
+  /**
+   * Main initialization function. Do not perform any initialization here.
+   *
+   * <p>
+   * If you change your main robot class, change the parameter type.
+   */
+  public static void main(String... args) {}
+}

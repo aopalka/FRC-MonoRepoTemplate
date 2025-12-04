31e303131ebfd5699a1aa5bcc434eb608c5825e0
aopalka <79727669+aopalka@users.noreply.github.com>
2025-12-04 17:37:43 -0500

    Build

--- comp/Template/src/main/java/frc/robot/BuildConstants.java
+++ comp/Template/src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Template";
+  public static final String MAVEN_NAME = "template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "0e8dc76427178b5967f69d3768b9657f71a55287";
-  public static final String GIT_DATE = "2025-12-01 20:25:26 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "65c7fc75df2c2d80166432fceeca5fa177ad40c5";
+  public static final String GIT_DATE = "2025-12-01 21:43:42 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-12-01 21:39:02 EST";
-  public static final long BUILD_UNIX_TIME = 1764643142728L;
+  public static final String BUILD_DATE = "2025-12-03 16:20:33 EST";
+  public static final long BUILD_UNIX_TIME = 1764796833116L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

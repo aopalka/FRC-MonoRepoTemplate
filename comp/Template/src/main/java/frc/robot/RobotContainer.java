db7e414f50c9494035826093e5861907a136f2d3
aopalka <79727669+aopalka@users.noreply.github.com>
2026-01-09 17:45:02 -0500

    Update RobotContainer.java

--- comp/Template/src/main/java/frc/robot/RobotContainer.java
+++ comp/Template/src/main/java/frc/robot/RobotContainer.java
@@ -22,8 +22,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 // import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;

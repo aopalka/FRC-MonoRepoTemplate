c958f94e85bd685069c02001205d674fa323cbbd
aopalka <79727669+aopalka@users.noreply.github.com>
2026-01-12 20:19:34 -0500

    PathPlanner!

--- comp/Template/src/main/java/frc/robot/BuildConstants.java
+++ comp/Template/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "235ac5100a97975ee96f832dda81676aa7599c4a";
-  public static final String GIT_DATE = "2026-01-12 19:03:33 EST";
+  public static final int GIT_REVISION = 27;
+  public static final String GIT_SHA = "0bf56e5f2bafde483a0e647d8c81ef0feea52765";
+  public static final String GIT_DATE = "2026-01-12 19:55:56 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-12 19:53:42 EST";
-  public static final long BUILD_UNIX_TIME = 1768265622269L;
+  public static final String BUILD_DATE = "2026-01-12 20:16:00 EST";
+  public static final long BUILD_UNIX_TIME = 1768266960199L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

0bf56e5f2bafde483a0e647d8c81ef0feea52765
aopalka <79727669+aopalka@users.noreply.github.com>
2026-01-12 19:55:56 -0500

    Upgrading VendorLibs!

--- comp/Template/src/main/java/frc/robot/BuildConstants.java
+++ comp/Template/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 25;
-  public static final String GIT_SHA = "92935c5f500e7406d772b26859bda464dc7b555f";
-  public static final String GIT_DATE = "2026-01-11 11:17:55 EST";
+  public static final int GIT_REVISION = 26;
+  public static final String GIT_SHA = "235ac5100a97975ee96f832dda81676aa7599c4a";
+  public static final String GIT_DATE = "2026-01-12 19:03:33 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-12 18:59:33 EST";
-  public static final long BUILD_UNIX_TIME = 1768262373083L;
+  public static final String BUILD_DATE = "2026-01-12 19:53:42 EST";
+  public static final long BUILD_UNIX_TIME = 1768265622269L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
